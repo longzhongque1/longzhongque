78ab3cac84fe0eaf0e67ee5a78f155ed2b9e25cc
Fairytail <469678918@qq.com>
2026-03-24 12:32:28 +0800

    项目初始化

---
+++ src/main/java/com/example/aicodeproductbackend/AiCodeProductBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicodeproductbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeProductBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeProductBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aicodeproductbackend/AiCodeProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicodeproductbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
