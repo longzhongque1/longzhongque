c4c890759303fb48e82c0f19f709563f202a369c
Fairytail <469678918@qq.com>
2026-03-25 10:48:15 +0800

    后端用户管理模块开发

--- src/main/java/com/example/aicodeproductbackend/AiCodeProductBackendApplication.java
+++ src/main/java/com/example/aicodeproductbackend/AiCodeProductBackendApplication.java
@@ -1,9 +1,12 @@
 package com.example.aicodeproductbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.example.aicodeproductbackend.mapper")
 public class AiCodeProductBackendApplication {
 
 	public static void main(String[] args) {
